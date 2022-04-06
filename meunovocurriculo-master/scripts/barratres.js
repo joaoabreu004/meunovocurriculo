4782957294ffabc94a1fcda4ea438ab203d2bd80
João Abreu <jvabreusousa14@gmail.com>
2022-04-06 19:25:49 -0300

    Alteração no footer do card.
    alterações feitas no senai.

---
+++ meunovocurriculo-master/scripts/barratres.js
@@ -0,0 +1,17 @@
+let barraProgresso3  = document.querySelector(".barra-progresso__JS"); 
+let valorObtido3 = document.querySelector(".valor__3"); 
+
+let progressoValor3 = 0; 
+let progressoValorFinal3 = 20; 
+let velocidade3 = 10; 
+
+let progresso3 = setInterval(() =>{
+    progressoValor3++;
+    console.log(progressoValor3);
+    valorObtido3.textContent = `${progressoValor3}%`;
+
+    if(progressoValor3 == progressoValorFinal3){
+        clearInterval(progresso3); 
+    }
+
+}, velocidade3)
