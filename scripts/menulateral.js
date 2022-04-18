0fb0d72d1eee4335efecd1a9c26308dd0ddf49fe
João Vitor de Abreu <jvabreusousa14@gmail.com>
2022-04-18 14:43:01 -0300

    adiconando nova estilizão quando o btn for clicado

--- scripts/menulateral.js
+++ scripts/menulateral.js
@@ -1,12 +1,14 @@
 const botaoFechar = document.querySelector('.btnclose'); 
 const informacoes = document.querySelector('.card__lateral'); 
-const deslocTextos = document.querySelector('.textos'); 
+var deslocTextos = document.querySelector('.textos'); 
 
 botaoFechar.addEventListener('click', () => {
     informacoes.classList.remove('card__lateral--ativo'); 
+    deslocTextos.classList.remove('textos--cardAberto')
 })
 
 const botaoAbrir = document.querySelector('.btnabrirmenu')
 botaoAbrir.addEventListener('click', () => {
     informacoes.classList.toggle('card__lateral--ativo');
+    deslocTextos.classList.add('textos--cardAberto');
 })
